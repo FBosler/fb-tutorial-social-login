ed88836fc903cf1ab1a2c1855d892db020f06e1a
Fabian Bosler <fbosler@gmail.com>
2020-01-31 00:13:49 +0100

    restored blank starting version

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,6 @@
 import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
-import { GiThreeFriends } from "react-icons/gi";
 
 function App() {
     return (
@@ -9,7 +8,7 @@ function App() {
             <header className="App-header">
                 <img src={logo} className="App-logo" alt="logo" />
                 <p>
-                    Edit <code>src/App.js</code> and save to reload. <GiThreeFriends />
+                    Edit <code>src/App.js</code> and save to reload.
                 </p>
                 <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
                     Learn React
