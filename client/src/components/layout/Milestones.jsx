49b92f7dce5708dc4a61f20ff90b055c5300e8f9
Fabian Bosler <fbosler@gmail.com>
2020-02-02 16:11:16 +0100

    changed snake case to camel case

--- client/src/components/layout/Milestones.jsx
+++ client/src/components/layout/Milestones.jsx
@@ -9,29 +9,29 @@ const ACHIEVED_COLOR = "#049e51";
 const OPEN_COLOR = "grey";
 
 // list of our milestones, can dynamically be extended or shortened, depending on preferences.
-// image_location   := location of the image file
-// lower_threshold  := min threshold to start working towards the milestones
-// upper_threshold  := threshold that needs to be reached to achieve the milestone
-const milestone_list = [
+// imageLocation   := location of the image file
+// lowerThreshold  := min threshold to start working towards the milestones
+// upperThreshold  := threshold that needs to be reached to achieve the milestone
+const milestoneList = [
     {
-        image_location: "assets/1_kitten.jpg",
-        lower_threshold: 0,
-        upper_threshold: 5
+        imageLocation: "assets/1_kitten.jpg",
+        lowerThreshold: 0,
+        upperThreshold: 5
     },
     {
-        image_location: "assets/2_kitten.jpg",
-        lower_threshold: 5,
-        upper_threshold: 10
+        imageLocation: "assets/2_kitten.jpg",
+        lowerThreshold: 5,
+        upperThreshold: 10
     },
     {
-        image_location: "assets/3_kitten.jpg",
-        lower_threshold: 10,
-        upper_threshold: 20
+        imageLocation: "assets/3_kitten.jpg",
+        lowerThreshold: 10,
+        upperThreshold: 20
     }
 ];
 
 // this is a trick based on https://stackoverflow.com/questions/1495407/maintain-the-aspect-ratio-of-a-div-with-css to force an aspect ratio of 16:9.
-const aspect_ratio = {
+const aspectRatio = {
     display: "block",
     width: "100%",
     position: "relative",
@@ -40,7 +40,7 @@ const aspect_ratio = {
 };
 
 // absolut positioning with pos 0,0,0,0 is also required for the aspect ratio to work
-const col_style = {
+const colStyle = {
     position: "absolute",
     left: 0,
     bottom: 0,
@@ -52,7 +52,7 @@ const col_style = {
 };
 
 // turns the elemnt into a round one, that is placed in the, you guessed it, top-right corner
-const top_right_corner = {
+const topRightCorner = {
     position: "absolute",
     width: "40px",
     height: "40px",
@@ -64,38 +64,38 @@ const top_right_corner = {
 
 // Text that will be displayed within our circular progress bar, coloring depends on level of achievment
 
-const ProgressBarText = (upper_threshold, value, percent) => {
+const ProgressBarText = (upperThreshold, value, percent) => {
     const color = percent <= 0 ? OPEN_COLOR : ACHIEVED_COLOR;
 
     if (percent < 0) {
-        return <span style={{ color, fontSize: "12px" }}>{upper_threshold}</span>;
+        return <span style={{ color, fontSize: "12px" }}>{upperThreshold}</span>;
     } else if (percent >= 100) {
         return (
             <span style={{ color, fontSize: "12px" }}>
-                {upper_threshold} <MdCheck style={{ color }} />
+                {upperThreshold} <MdCheck style={{ color }} />
             </span>
         );
     } else {
         return (
             <span style={{ color, fontSize: "12px" }}>
-                {value}/{upper_threshold}
+                {value}/{upperThreshold}
             </span>
         );
     }
 };
 
 // A milestone component, including the circular progress bar
-const Milestone = (url, lower_threshold, upper_threshold, referrals) => {
-    const required_referrals = upper_threshold - lower_threshold;
-    const achieved_referrals = referrals - lower_threshold;
+const Milestone = (url, lowerThreshold, upperThreshold, referrals) => {
+    const required_referrals = upperThreshold - lowerThreshold;
+    const achieved_referrals = referrals - lowerThreshold;
     const percentage = (achieved_referrals / required_referrals) * 100;
-    const text = ProgressBarText(upper_threshold, referrals, percentage);
+    const text = ProgressBarText(upperThreshold, referrals, percentage);
 
     return (
         <div style={{ paddingTop: "25px" }}>
-            <div style={aspect_ratio}>
-                <div className="z-depth-4" style={{ ...col_style, backgroundImage: `url("${url}")` }}>
-                    <div style={top_right_corner}>
+            <div style={aspectRatio}>
+                <div className="z-depth-4" style={{ ...colStyle, backgroundImage: `url("${url}")` }}>
+                    <div style={topRightCorner}>
                         <CircularProgressbarWithChildren
                             value={percentage}
                             strokeWidth={10}
@@ -121,10 +121,10 @@ const Milestones = props => {
     return (
         <Container>
             <Row>
-                {milestone_list.map(function(item) {
+                {milestoneList.map(function(item) {
                     return (
                         <Col xs={12} sm={6} md={4}>
-                            {Milestone(item.image_location, item.lower_threshold, item.upper_threshold, referrals)}
+                            {Milestone(item.imageLocation, item.lowerThreshold, item.upperThreshold, referrals)}
                         </Col>
                     );
                 })}
