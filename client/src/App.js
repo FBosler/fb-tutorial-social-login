917a3053f1b7b4d82833812ac8236d3774d1e40a
Fabian Bosler <fbosler@gmail.com>
2020-02-06 07:51:52 +0100

    first v2 version

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,23 @@
 import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import Milestones from "./components/layout/Milestones";
+import About from "./components/layout/About";
+
 import "bootstrap/dist/css/bootstrap.min.css";
 
 const App = () => {
-    return <Milestones referrals={12} />;
+    return (
+        <Router>
+            <div className="App">
+                <Route exact path="/">
+                    <Milestones referrals={12} />
+                </Route>
+                <Route exact path="/about">
+                    <About />
+                </Route>
+            </div>
+        </Router>
+    );
 };
 
 export default App;
---
+++ client/src/components/layout/About.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const About = () => {
+    return <div>This is our about page</div>;
+};
+
+export default About;
